ed848954800e1fbf62883c6e73fcf7fa10375c4c
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-07 00:30:22 +0530

    EID_cc

--- EID_cc.java
+++ EID_cc.java
@@ -22,11 +22,8 @@ public class EID_cc {
 				for (i = 0; i < n - 1; i++) {
 					d[i] = abs(v[i] - v[i + 1]);
 				}
-				for (i = 0; i < n - 1; i++) {
-					System.out.printf("\n%d",d[i]);
-				}
 				s.sort(d, n - 1);
-				System.out.printf("\n%d", d[0]);
+				System.out.printf("%d\n", d[0]);
 				t--;
 			}
 		}
@@ -34,7 +31,7 @@ public class EID_cc {
 
 	public void sort(int[] a, int b) {
 		int l, m, tmp;
-		for (l = 1; l <= b; l++) {
+		for (l = 1; l < b; l++) {
 			for (m = 0; m < l; m++) {
 				if (a[m] > a[l]) {
 					tmp = a[l];
