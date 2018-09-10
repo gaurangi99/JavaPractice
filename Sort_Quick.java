98f304b4cbf6c5ef6c93faa98a0e1d75f761236e
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-10 23:37:46 +0530

    c

---
+++ Sort_Quick.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author RK
+ *
+ */
+public class Sort_Quick {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
