07e6ae0894fd7879849d5ee62dfb409b0794ab6b
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-11 22:08:37 +0530

    change

--- cc_MAGICHF.java
+++ cc_MAGICHF.java
@@ -3,15 +3,44 @@ import java.util.*;
 public class cc_MAGICHF {
 
 	public static void main(String[] args) {
-		int t;
-		cc_MAGICHF mc = new cc_MAGICHF();
+		int t, n, x, s, i;
+		int[] a = new int[10];
+		int[] b = new int[10];
+		// cc_MAGICHF mc = new cc_MAGICHF();
 		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
 			t = in.nextInt();
-			
+			while (t > 0) {
+				n = in.nextInt();
+				x = in.nextInt();
+				s = in.nextInt();
+				System.out.println();
+				for (i = s; i > 0; i--) {
+					a[i] = in.nextInt();
+					b[i] = in.nextInt();
+					System.out.print("\n");
+				}
+				while (x <= n) {
+					for (i = s; i > 0; i--) {
+						if (a[i] <= n && b[i] <= n)
+							;
+						else {
+							System.exit(0);
+						}
+					}
+				}
+				for (i = s; i > 0; i--) {
+					do {
+						if (a[i] == x) {
+							x = b[i];
+						} else if (b[i] == x) {
+							x = a[i];
+						}
+					} while (a[i] != b[i]);
+				}
+				System.out.printf("%d", x);
+			}
 		}
-
-		// TODO Auto-generated method stub
-
-	}
+	}// TODO Auto-generated method stub
 
 }
