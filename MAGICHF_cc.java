593a71b1447dc60e528d40257c5a87bb1a4bf5b6
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-11 20:09:06 +0530

    to be deleted

--- MAGICHF_cc.java
+++
@@ -1,9 +0,0 @@
-
-public class magichf {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- SQMANIA_cc.java
+++
@@ -1,8 +0,0 @@
-public class sqmania {
-
-	public static void main(String[] args) {
-		int // TODO Auto-generated method stub
-
-	}
-
-}
