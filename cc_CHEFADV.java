6986cf25a0fc7bfe435f55946b032720246f59ef
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-13 20:59:01 +0530

    change

--- cc_CHEFADV.java
+++ cc_CHEFADV.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class cc_CHEFADV {
 
 	public static void main(String[] args) {
-		int t, n, m, x, y, power = 1, knowledge = 1;
+		int t, n, m, x, y;
 		try (Scanner in = new Scanner(System.in);) {
 			System.out.println();
 			t = in.nextInt();
-			while (t > 0) {
+			while (t>0) {
+				int power = 1, knowledge = 1;
 				n = in.nextInt();
 				m = in.nextInt();
 				x = in.nextInt();
@@ -15,7 +16,8 @@ public class cc_CHEFADV {
 				if (n == 1 && m == 1) {
 					System.out.println("Chefirnemo");
 				} else if (n == 2 && m == 2) {
-					System.out.println("Chefirnemo");
+					power++;
+					knowledge++;
 				} else if (n > 2 && m > 2) {
 					if (n % x == 0 && m % y == 0) {
 						while (power != n) {
@@ -24,18 +26,21 @@ public class cc_CHEFADV {
 						while (knowledge != m) {
 							knowledge += y;
 						}
-					} else if (n % x == 1 && m % y == 1) {
-						while (power <= n) {
+					} else if (n % x != 0 && m % y != 0) {
+						while (power < n && (n - power) != 1) {
 							power += x;
 						}
-						while (knowledge <= n) {
+						while (knowledge < m && (m - knowledge) != 1) {
 							knowledge += y;
 						}
-						if(power!=n && knowledge!=m) {
+						if (power != n && knowledge != m) {
 							power++;
 							knowledge++;
-							}
 						}
+					}
+				} /*
+					 * else { System.out.println("Pofik"); }}
+					 */
 				if (power == n && knowledge == m) {
 					System.out.println("Chefirnemo");
 				} else {
