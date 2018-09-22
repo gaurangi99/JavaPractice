b081541ac73fca2aea2a7ac1ed8dd7a54f2797be
gaurangi99 <imgaurangi.99@gmail.com>
2018-09-22 17:42:57 +0530

    change

--- cc_MAGICHF.java
+++ cc_MAGICHF.java
@@ -3,36 +3,30 @@ import java.util.*;
 public class cc_MAGICHF {
 
 	public static void main(String[] args) {
-		int t, n, x, s, i, flag = 0;
-		int[] a = new int[10];
-		int[] b = new int[10];
-		// cc_MAGICHF mc = new cc_MAGICHF();
+		int t, n, x, s;
+		int a, b;
 		try (Scanner in = new Scanner(System.in);) {
 			System.out.println();
 			t = in.nextInt();
-			while (t > 0 && t <= 100) {
+			while (t > 0) {
 				n = in.nextInt();
 				x = in.nextInt();
 				s = in.nextInt();
-				flag += s;
-				for (i = s; i > 0; i--) {
-					a[i] = in.nextInt();
-					b[i] = in.nextInt();
-				}
-				if (flag <= 200000) {
-					if (n >= 2 && n <= 100000 && x <= n) {
-						for (i = s; i > 0; i--) {
-							if ((a[i] <= n && b[i] <= n) && (a[i] != b[i])) {
-								if (a[i] == x) {
-									x = b[i];
-								} else if (b[i] == x) {
-									x = a[i];
-								}
+				while (s > 0) {
+					a = in.nextInt();
+					b = in.nextInt();
+					if (x <= n) {
+						if ((a <= n && b <= n) && (a != b)) {
+							if (a == x) {
+								x = b;
+							} else if (b == x) {
+								x = a;
 							}
 						}
 					}
+					s--;
 				}
-				System.out.printf("%d", x);
+				System.out.println(x);
 				t--;
 			}
 		}
