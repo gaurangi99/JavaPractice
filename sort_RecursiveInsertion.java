c2c17b96edd190648717a9374f8d487ed94997d9
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-02 00:59:30 +0530

    change

--- sort_RecursiveInsertion.java
+++ sort_RecursiveInsertion.java
@@ -10,12 +10,12 @@ public class sort_RecursiveInsertion {
 			System.out.println("Enter size of array:");
 			n = in.nextInt();
 			System.out.println("Array's elements are:");
-			for (i = 0; i < n; i++) {
+			for (i = 1; i <= n; i++) {
 				a[i] = in.nextInt();
 			}
 			ri.recursive_insertion(a, n);
 			System.out.println("\nSorted array is:\n");
-			for (i = 0; i < n; i++) {
+			for (i = 1; i <= n; i++) {
 				System.out.printf("%d ", a[i]);
 			}
 		}
@@ -28,7 +28,7 @@ public class sort_RecursiveInsertion {
 		}
 		key = a[n];
 		i = n - 1;
-		while (i > -1 && a[i] > key) {
+		while (i > 0 && a[i] > key) {
 			a[i + 1] = a[i];
 			i--;
 		}
