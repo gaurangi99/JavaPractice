bb21ca11260abafd7ef020e3e18ca5dfa112e7f2
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-02 20:08:35 +0530

    incomplete

---
+++ cc_ZCO14001.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class cc_ZCO14001 {
+
+	public static void main(String[] args) {
+		int n, h, i;
+		int[] s = new int[10];
+		
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			n = in.nextInt();
+			h = in.nextInt();
+			for (i = 0; i < n; i++) {
+				s[i] = in.nextInt();
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
