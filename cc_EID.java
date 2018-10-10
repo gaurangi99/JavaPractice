2f195738bc8645d26f01f726ac3e7542ced97ecf
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-10 13:20:11 +0530

    commit

--- cc_EID.java
+++ cc_EID.java
@@ -1,93 +1,32 @@
-import java.util.Scanner;
-import static java.lang.Math.abs;
+import java.util.*;
 
 public class cc_EID {
-	int[] a = new int[10];
-	int b;
 
 	public static void main(String[] args) {
-		int t, i;
-		long n;
+		int t, i, n;
 		int[] v = new int[10];
 		int[] d = new int[10];
-		System.out.println();
 		try (Scanner in = new Scanner(System.in);) {
-			cc_EID s = new cc_EID();
+			System.out.println();
 			t = in.nextInt();
 			while (t > 0) {
 				n = in.nextInt();
 				for (i = 0; i < n; i++) {
 					v[i] = in.nextInt();
 				}
-				s.sort(v, n);
+				Arrays.sort(v);
 				for (i = 0; i < n - 1; i++) {
-					d[i] = abs(v[i + 1] - v[i]);
+					d[i] = v[i + 1] - v[i];
 				}
-				s.sort(d, n - 1);
-				System.out.printf("%d\n", d[0]);
+				Arrays.sort(d);
+				System.out.println(d[0]);
 				t--;
 			}
 		}
 	}
-
-	public void sort(int[] a, long b) {
-		int l, m, tmp;
-		for (l = 1; l < b; l++) {
-			for (m = 0; m < l; m++) {
-				if (a[m] > a[l]) {
-					tmp = a[l];
-					a[l] = a[m];
-					a[m] = tmp;
-				}
-			}
-		}
-	}
-	// TODO Auto-generated method stub
 }
-
-/*import java.util.*;
-import java.lang.*;
-import java.io.*;
-import static java.lang.Math.abs;
-
-//Name of the class has to be "Main" only if the class is public.
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    int t, i;
-	    long n;
-		int[] v = new int[10];
-		int[] d = new int[10];
-		System.out.println();
-		try (Scanner in = new Scanner(System.in);) {
-			Codechef s = new Codechef();
-			t = in.nextInt();
-			while (t > 0) {
-				n = in.nextInt();
-				for (i = 0; i < n; i++) {
-					v[i] = in.nextInt();
-				}
-				s.sort(v, n);
-				for (i = 0; i < n - 1; i++) {
-					d[i] = abs(v[i] - v[i + 1]);
-				}
-				s.sort(d, n - 1);
-				System.out.printf("%d\n", d[0]);
-				t--;
-			}
-		}	// your code goes here
-	}
-	public void sort(int[] a, long b) {
-		int l, m, tmp;
-		for (l = 1; l < b; l++) {
-			for (m = 0; m < l; m++) {
-				if (a[m] > a[l]) {
-					tmp = a[l];
-					a[l] = a[m];
-					a[m] = tmp;
-				}
-			}
-		}
-	}
-}*/
+/*
+ * public void sort(int[] a, int b) { int j, k, key; for (j = 1; j < b; j++) {
+ * key = a[j]; k = j - 1; while (k > -1 && key < a[k]) { a[k + 1] = a[k]; k--; }
+ * a[k + 1] = key; } // TODO Auto-generated method stub }
+ */
