f014a8c13a4d7c671e5905e4fd1f10723acb74c0
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-10 10:31:25 +0530

    final change

--- cc_SINS.java
+++ cc_SINS.java
@@ -3,25 +3,35 @@ import java.util.*;
 public class cc_SINS {
 
 	public static void main(String[] args) {
-		int t, x, y, s = 0;
+		int t, x, y, s = 0, r, a = 0, b = 0;
 		try (Scanner in = new Scanner(System.in);) {
 			System.out.println();
 			t = in.nextInt();
 			while (t > 0) {
 				x = in.nextInt();
 				y = in.nextInt();
-				while (x != y && (x != 0 || y != 0)) {
-					if (x < y) {
-						y = y - x;
-					} else if (x > y) {
-						x = x - y;
+				if (x != 0 && y != 0) {
+					if (x > y) {
+						a = x;
+						b = y;
+					} else if (x < y) {
+						a = y;
+						b = x;
+					} else if (x == y) {
+						a = b = x;
 					}
+					while (a % b != 0) {
+						r = a % b;
+						a = b;
+						b = r;
+					}
+					s = 2 * b;
+				} else {
+					s = x + y;
 				}
-				s = x + y;
 				System.out.println(s);
 				t--;
 			}
-			// TODO Auto-generated method stub
 		}
 	}
 }
