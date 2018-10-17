17ef6de7304a133ffc6e4921c7ad290e0fd3b166
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-18 03:20:32 +0530

    commit

---
+++ concept_ClassObj.java
@@ -0,0 +1,12 @@
+public class concept_ClassObj {
+	public static void main(String[] args) {
+		concept_ClassObj co = new concept_ClassObj();
+		System.out.println(co.sum(68, 7));
+	}
+
+	public int sum(int a, int b) {
+		int s;
+		s = a + b;
+		return s;
+	}
+}
