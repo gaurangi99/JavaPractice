d938c5ed8971e720cdb2a7b1ae4eb9d38eab604d
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-18 03:21:24 +0530

    commit

---
+++ concept_StaticMemFunc.java
@@ -0,0 +1,11 @@
+public class concept_StaticMemFunc {
+
+	public static void main(String[] args) {
+		System.out.println(sum(6, 7));
+	}
+
+	public static int sum(int a, int b) {
+		int s = a + b;
+		return s;
+	}
+}
