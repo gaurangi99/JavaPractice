3e4210cbed68d7893a50341ef6d08aa439bbc7bc
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-20 16:45:47 +0530

    commit

---
+++ Sort_InsertionRecursive.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Sort_InsertionRecursive {
+
+	public static void main(String[] args) {
+		int n, i;
+		int[] a = new int[10];
+		try (Scanner in = new Scanner(System.in);) {
+			Sort_InsertionRecursive ri = new Sort_InsertionRecursive();
+			System.out.println("Enter size of array:");
+			n = in.nextInt();
+			System.out.println("Array's elements are:");
+			for (i = 1; i <= n; i++) {
+				a[i] = in.nextInt();
+			}
+			ri.recursive_insertion(a, n);
+			System.out.println("\nSorted array is:\n");
+			for (i = 1; i <= n; i++) {
+				System.out.printf("%d ", a[i]);
+			}
+		}
+	}
+
+	public void recursive_insertion(int a[], int n) {
+		int key, i;
+		if (n != 1) {
+			recursive_insertion(a, n - 1);
+		}
+		key = a[n];
+		i = n - 1;
+		while (i > 0 && a[i] > key) {
+			a[i + 1] = a[i];
+			i--;
+		}
+		a[i + 1] = key;
+	}// TODO Auto-generated method stub
+}
