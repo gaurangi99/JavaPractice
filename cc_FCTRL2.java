7f68e9f8a7a576f50b413431db0c75a544564816
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-21 01:55:23 +0530

    commit

--- cc_FCTRL2.java
+++ cc_FCTRL2.java
@@ -1,20 +1,17 @@
 import java.util.*;
+import java.math.*;
 
 public class cc_FCTRL2 {
 
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in);) {
 			System.out.println();
-			BigNumber t = in.nextInt();
+			int t = in.nextInt();
 			while (t > 0) {
-				BigNumber i = 0, fac = 1;
-				BigNumber n = in.nextInt();
-				if (n == 0) {
-					fac = 1;
-				} else {
-					for (i = n; i >= 1; i--) {
-						fac = fac * i;
-					}
+				BigInteger fac = new BigInteger("1");
+				int n = in.nextInt();
+				for (int i = 2; i <= n; i++) {
+					fac = fac.multiply(BigInteger.valueOf(i));
 				}
 				System.out.println(fac);
 				t--;
