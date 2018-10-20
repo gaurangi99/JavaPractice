97a491b2a22519a2c8462baaf578dbf92765f51c
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-21 03:24:01 +0530

    commit

--- cc_HS08TEST.java
+++ cc_HS08TEST.java
@@ -4,14 +4,16 @@ public class cc_HS08TEST {
 
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
 			int x = in.nextInt();
-			float bc = (1 / 2);
-			float ab = in.nextFloat();
+			double bc = 0.50;
+			System.out.println(bc);
+			double ab = in.nextDouble();
 			if (x % 5 == 0) {
 				ab = ab - x - bc;
-				System.out.printf("%.2f", ab);
+				System.out.printf("%.2d", ab);
 			} else {
-				System.out.printf("%.2f", ab);
+				System.out.printf("%.2d", ab);
 			}
 			// TODO Auto-generated method stub
 		}
