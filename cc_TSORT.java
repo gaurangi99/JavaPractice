c92aceadc98a0d247aee55b2544fe2234442a820
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-21 02:24:02 +0530

    commit

--- cc_TSORT.java
+++ cc_TSORT.java
@@ -5,31 +5,16 @@ public class cc_TSORT {
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in);) {
 			System.out.println();
+			int k;
 			int t = in.nextInt();
 			int a[] = new int[t];
-			for (int k = 0; k < t; k++)
+			for (k = 0; k < t; k++)
 				a[k] = in.nextInt();
-			insertionSort(t, a);
-			sorted_Arr(t, a);
-			// TODO Auto-generated method stub
-		}
-	}
-
-	public static void insertionSort(int t, int[] a) {
-		int i, j, tmp;
-		for (i = 1; i < t; i++)
-			for (j = 0; j < i; j++) {
-				if (a[j] > a[i]) {
-					tmp = a[j];
-					a[j] = a[i];
-					a[i] = tmp;
-				}
+			Arrays.sort(a);
+			for (k = 0; k < t; k++) {
+				System.out.println(a[k]);
 			}
-	}
-
-	public static void sorted_Arr(int t, int[] a) {
-		for (int i = 0; i < t; i++) {
-			System.out.println(a[i]);
+			// TODO Auto-generated method stub
 		}
 	}
 }
