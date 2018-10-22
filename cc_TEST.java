f2950afd47ec5609c83eacbf5e5b734aed9f96e0
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-22 20:53:08 +0530

    commit

--- cc_TEST.java
+++ cc_TEST.java
@@ -4,13 +4,14 @@ public class cc_TEST {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int i;
-		do {
-			i = in.nextInt();
-			while (i <= 42) {
+		while (true) {
+			int i = in.nextInt();
+			if (i != 42) {
 				System.out.println(i);
+			} else {
+				break;
 			}
-		} while (i <= 99);
-		// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+		}
 	}
 }
