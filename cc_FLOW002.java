b8a479e42530934cdcbd25a55b72db61f9fe5c9e
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-22 18:43:44 +0530

    commit

---
+++ cc_FLOW002.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class cc_FLOW002 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t > 0) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int r;
+			r = a % b;
+			System.out.println(r);
+			t--;
+		}
+		// TODO Auto-generated method stub
+	}
+}
