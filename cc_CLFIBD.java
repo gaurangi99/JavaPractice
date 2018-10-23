a62c87f66c6fe94edcd599832eb4981032b14be2
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-23 20:46:31 +0530

    commit

---
+++ cc_CLFIBD.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class cc_CLFIBD {
+
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in);) {
+			int t = in.nextInt();
+			while (t > 0) {
+				char[] s = new char[100000];
+				int[] c = new int[26];
+				int i;
+				if (s.length <= 2) {
+					System.out.println("Dynamic");
+				} else {
+					for (i = 0; i < s.length; i++) {
+
+					}
+				}
+				t--;
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
