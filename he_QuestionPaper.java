81bca2b6f98e0ff748906e06422ded1290d88f35
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-28 20:26:20 +0530

    commit

--- he_QuestionPaper.java
+++ he_QuestionPaper.java
@@ -3,20 +3,22 @@ import java.util.*;
 public class he_QuestionPaper {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		long t = in.nextLong();
-		while (t > 0) {
-			long n = in.nextLong();
-			int a = in.nextInt();
-			int b = in.nextInt();
-			long r;
-			if (a == b) {
-				r = (n * n) + 1;
-			} else {
-				r = ((n + 2) * (n + 1)) / 2;
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			long t = in.nextLong();
+			while (t > 0) {
+				long n = in.nextLong();
+				int a = in.nextInt();
+				int b = in.nextInt();
+				long r = 0;
+				if (a == b) {
+					System.out.println((n * 2) + 1);
+				} else {
+					r = (n + 2) * (n + 1) / 2;
+				}
+				System.out.println(r);
+				t--;
 			}
-			System.out.println(r);
-			t--;
 		}
 		// TODO Auto-generated method stub
 	}
