5fd54e7c483d7876726faa48af0c62d84d0d8ba2
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-29 00:25:58 +0530

    commit

--- he_TotalCost.java
+++ he_TotalCost.java
@@ -3,26 +3,28 @@ import java.util.*;
 public class he_TotalCost {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int p = in.nextInt();
-		int s = in.nextInt();
-		int t = in.nextInt();
-		int h = in.nextInt();
-		int x = in.nextInt();
-		int tc = 0;
-		if (x <= s) {
-			while (s > t) {
-				tc += p;
-				s--;
-				x--;
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			int p = in.nextInt();
+			int s = in.nextInt();
+			int t = in.nextInt();
+			int h = in.nextInt();
+			int x = in.nextInt();
+			int tc = 0;
+			if (x <= s) {
+				while (s > t && x > 0) {
+					tc += p;
+					s--;
+					x--;
+				}
+				while (s > 0 && s <= t && x > 0) {
+					tc += h;
+					s--;
+					x--;
+				}
 			}
-			while (s > 0 && s <= t && x > 0) {
-				tc += h;
-				s--;
-				x--;
-			}
+			System.out.println(tc);
 		}
-		System.out.println(tc);
 		// TODO Auto-generated method stub
 	}
 }
