7e9c15d6d606f2c532f8992cc8e591bcfb7f48eb
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-29 23:11:43 +0530

    commit

--- cc_AVGMAT.java
+++ cc_AVGMAT.java
@@ -1,7 +1,20 @@
+import java.util.*;
 
 public class cc_AVGMAT {
 
 	public static void main(String[] args) {
+		try(Scanner in=new Scanner(System.in);){
+			int t=in.nextInt();
+			while(t>0) {
+				int n=in.nextInt();
+				int m=in.nextInt();
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=m;j++) {
+						String s
+					}
+				}
+			}
+		}
 		// TODO Auto-generated method stub
 
 	}
