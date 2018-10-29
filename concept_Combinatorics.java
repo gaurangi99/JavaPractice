fbffefb32875be3a01730789ca87d0cd7139b148
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-29 12:19:17 +0530

    commit

---
+++ concept_Combinatorics.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class concept_Combinatorics {
+	
+	static HashMap combo(int n) {
+		
+		HashMap result = new HashMap(100);
+
+		for (int j = 1; j < n; j++) {
+			for (int i = 1; i <= n; i++) {
+				int m = ((i + j) - 1) % n + 1;
+				if (i < m) {
+					int[] v = { i, m };
+					result.put(i + "," + m, v);
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		HashMap result = new HashMap(100);
+		result = combo(5);
+		System.out.println(result.toString());
+	}
+}
