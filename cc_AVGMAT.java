6c15519b4a47e9b630398009740113c2a6e1ac53
gaurangi99 <imgaurangi.99@gmail.com>
2018-10-29 23:19:32 +0530

    commit

--- cc_AVGMAT.java
+++ cc_AVGMAT.java
@@ -3,14 +3,15 @@ import java.util.*;
 public class cc_AVGMAT {
 
 	public static void main(String[] args) {
-		try(Scanner in=new Scanner(System.in);){
-			int t=in.nextInt();
-			while(t>0) {
-				int n=in.nextInt();
-				int m=in.nextInt();
-				for(int i=1;i<=n;i++) {
-					for(int j=1;j<=m;j++) {
-						String s
+		try (Scanner in = new Scanner(System.in);) {
+			int t = in.nextInt();
+			while (t > 0) {
+				int n = in.nextInt();
+				int m = in.nextInt();
+				String[] s = new String[m];
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= m; j++) {
+
 					}
 				}
 			}
