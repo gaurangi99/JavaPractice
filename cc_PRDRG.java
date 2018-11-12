d9ec9ab443e6a671fd2473ec64d4461e7eae1fa2
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-12 11:58:14 +0530

    commit

--- cc_PRDRG.java
+++ cc_PRDRG.java
@@ -3,25 +3,28 @@ import java.util.*;
 public class cc_PRDRG {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int i;
-		int t = in.nextInt();
-		int[] n = new int[25];
-		double[] fr = new double[50];
-		for (i = 1; i <= t; i++) {
-			n[i] = in.nextInt();
-		}
-		for (i = 1; i <= 2 * t; i++) {
-			if (i % 2 != 0) {
-				fr[i] = 1;
-			} else {
-				int q = i / 2;
-				fr[i] = Math.pow(2, q);
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			int i;
+			int t = in.nextInt();
+			int[] n = new int[t];
+			double[] fr = new double[2 * t];
+			for (i = 0; i < t; i++) {
+				n[i] = in.nextInt();
 			}
+			System.out.println();
+			for (i = 0; i < 2 * t; i++) {
+				if (i % 2 == 0) {
+					fr[i] = 1;
+				} else {
+					int q = i / 2;
+					fr[i] = Math.pow(2, q + 1);
+				}
+			}
+			for (i = 0; i < 2 * t; i++) {
+				System.out.printf("%f ", fr[i]);
+			}
+			// TODO Auto-generated method stub
 		}
-		for (i = 1; i <= 2 * t; i++) {
-			System.out.printf("%d ", fr[i]);
-		}
-		// TODO Auto-generated method stub
 	}
 }
