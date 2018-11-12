9b0d1bb428010428997c8c9f98ceff2582622757
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-12 15:18:10 +0530

    commit

--- cc_PRDRG.java
+++ cc_PRDRG.java
@@ -7,8 +7,8 @@ public class cc_PRDRG {
 			System.out.println();
 			int i;
 			int t = in.nextInt();
-			int[] n = new int[t];
-			long[] fr = new long[2 * t];
+			int[] n = new int[25];
+			long[] fr = new long[50];
 			for (i = 0; i < t; i++) {
 				n[i] = in.nextInt();
 			}
@@ -16,19 +16,17 @@ public class cc_PRDRG {
 			fr[0] = fr[2] = 1;
 			fr[1] = 2;
 			fr[3] = 4;
-			for (i = 4; i < 2 * t; i++) {
-				if ((i / 2) % 2 == 0) {
-					fr[i] = fr[i - 2] * 2 + 1;
-				} else if ((i / 2) % 2 != 0) {
-					fr[i] = fr[i - 2] * 2 - 1;
-				} else if (i % 2 != 0) {
-					fr[i] = (long) Math.pow(2, (i / 2) + 1);
+			for (i = 4; i < 50; i++) {
+				if (i % 2 == 0) {
+					if ((i / 2) % 2 == 0) {
+						fr[i] = fr[i - 2] * 2 + 1;
+					} else if ((i / 2) % 2 != 0) {
+						fr[i] = fr[i - 2] * 2 - 1;
+					}
+				} else {
+					fr[i] = (long) Math.pow(2, (i / 2 + 1));
 				}
 			}
-			System.out.printf("%d ", fr[0]);
-			System.out.printf("%d ", fr[1]);
-			System.out.printf("%d ", fr[2]);
-			System.out.printf("%d ", fr[3]);
 			for (i = 0; i < t; i++) {
 				System.out.printf("%d ", fr[(2 * n[i]) - 2]);
 				System.out.printf("%d ", fr[(2 * n[i]) - 1]);
