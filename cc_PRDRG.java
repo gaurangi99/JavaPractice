807553de969085f0a327eb8d2ed392802c2156b6
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-12 11:22:01 +0530

    commit

---
+++ cc_PRDRG.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class cc_PRDRG {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		int[] n=new int[5];
+		int[] 
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
