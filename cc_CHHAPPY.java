cc2c48c4d475da67c56a63cbcaa1dffdacb30614
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-12 19:45:40 +0530

    commit

--- cc_CHHAPPY.java
+++ cc_CHHAPPY.java
@@ -3,25 +3,32 @@ import java.util.*;
 public class cc_CHHAPPY {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int t = in.nextInt();
-		while (t > 0) {
-			int i, j;
-			int n = in.nextInt();
-			int[] a = new int[n];
-			for (i = 0; i < n; i++) {
-				a[i] = in.nextInt();
-			}
-			for (i = 0; i < n - 1; i++) {
-				for (j = i + 1; j < n; j++) {
-					if (a[i] == a[j]) {
-
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			int t = in.nextInt();
+			while (t > 0) {
+				int i, j, flag = 0;
+				int n = in.nextInt();
+				int[] a = new int[n];
+				for (i = 0; i < n; i++) {
+					a[i] = in.nextInt();
+				}
+				for (i = 0; i < n - 1; i++) {
+					for (j = i + 1; j < n; j++) {
+						if (a[i] != a[j]) {
+							if (a[a[i]] == a[a[j]]) {
+								flag = 1;
+								System.out.println("Truly Happy");
+								System.exit(0);
+							}
+						}
 					}
+				}
+				if (flag == 0) {
+					System.out.println("Poor Chef");
 				}
 				t--; // TODO Auto-generated method stub
 			}
-
 		}
-
 	}
 }
