551a3c008e47bf44a298010a190cf7cf9636d84a
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-12 13:31:46 +0530

    commit

--- cc_PRDRG.java
+++ cc_PRDRG.java
@@ -8,22 +8,11 @@ public class cc_PRDRG {
 			int i;
 			int t = in.nextInt();
 			int[] n = new int[t];
-			double[] fr = new double[2 * t];
 			for (i = 0; i < t; i++) {
 				n[i] = in.nextInt();
 			}
 			System.out.println();
-			for (i = 0; i < 2 * t; i++) {
-				if (i % 2 == 0) {
-					fr[i] = 1;
-				} else {
-					int q = i / 2;
-					fr[i] = Math.pow(2, q + 1);
-				}
-			}
-			for (i = 0; i < 2 * t; i++) {
-				System.out.printf("%f ", fr[i]);
-			}
+			
 			// TODO Auto-generated method stub
 		}
 	}
