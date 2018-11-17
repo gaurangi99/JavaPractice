576c818ead199293727fe1adc407489ce1dfe9d8
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-17 16:40:00 +0530

    commit

--- cc_JOHNY.java
+++ cc_JOHNY.java
@@ -1,7 +1,11 @@
+import java.util.*;
 
 public class cc_JOHNY {
 
 	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in);) {
+			
+		}
 		// TODO Auto-generated method stub
 
 	}
