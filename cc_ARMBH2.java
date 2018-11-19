6591095752ed402043c9f861c0b7391d6fc28838
gaurangi99 <imgaurangi.99@gmail.com>
2018-11-19 23:59:25 +0530

    commit

---
+++ cc_ARMBH2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class cc_ARMBH2 {
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in);) {
+			System.out.println();
+			int t = in.nextInt();
+			while (t > 0) {
+				int n = in.nextInt();
+				int m = in.nextInt();
+				int k = in.nextInt();
+				long[][] a = new long[n][m];
+				int i,j,l;
+				long max = 0, sh = 0, sv = 0;
+				for (i = 0; i < n; i++) {
+					for (j = 0; j < m; j++) {
+						a[i][j] = in.nextLong();
+					}
+				}
+				for (i = 0; i < n; i++) {
+					for (j = 0; j <= m - k; j++) {
+						sh = a[i][j];
+						for (l = 1; l < k; l++) {
+							sh += a[i][j + l];
+						}
+						if (max < sh) {
+							max = sh;
+						}
+					}
+				}
+				for (i = 0; i < m; i++) {
+					for (j = 0; j <= n - k; j++) {
+						sv = a[i][j];
+						for (l = 1; l < k; l++) {
+							sv += a[i][j + l];
+						}
+						if (max < sv) {
+							max = sv;
+						}
+					}
+				}
+				System.out.println(max);
+				t--;
+			}
+		}
+		// TODO Auto-generated method stub
+	}
+}
