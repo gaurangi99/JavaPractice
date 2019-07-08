42937953b37539cbb032613dc09c6d0b7a5aeb89
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-08 22:17:00 +0530

    commit

---
+++ general.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class general {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		System.out.println();
+		int a;
+		System.out.println("dbvh");
+		System.out.println(a);
+	}
+}
---
+++ nptel_q2_11.java
@@ -0,0 +1,11 @@
+
+public class nptel_q2_11 {
+
+	public static void main(String[] args) {
+		int x=9;
+		if(x==9) {
+			//int y=8;
+		System.out.println(x);}
+		// TODO Auto-generated method stub
+	}
+}
