95584603b0438c930c7f76f29e36f1ff7fc17a89
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-16 12:33:16 +0530

    commit

---
+++ HugeIntegerTest.java
@@ -0,0 +1,12 @@
+public class HugeIntegerTest {
+
+	public static void main(String[] args) {
+		HugeInteger h = new HugeInteger();
+		HugeInteger h1 = new HugeInteger("2020438836736576577563757376872378192045");
+		HugeInteger h2 = new HugeInteger("9073621458237189450690736214582371894506");
+		HugeInteger h3 = new HugeInteger("9999999999999999999999999999999999999999");
+		HugeInteger h4 = new HugeInteger("1000000000000000000000000000000000000000");
+		
+		// TODO Auto-generated method stub
+	}
+}
