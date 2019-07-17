0d994da1ec816aee125db70eeb9c5e2eae7bdc91
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-17 15:59:24 +0530

    starting...

---
+++ Passenger.java
@@ -0,0 +1,9 @@
+public class Passenger {
+	String name, action;
+	String[] prevDestn = new String[3];
+	int passNum, actualAgriPdt, claimAgriPdt, actualWpn, claimWpn, actualMoney;
+	Date passIss;
+	char extraMoneyCheck;
+	
+	
+}
