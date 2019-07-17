b0c9dbba12373b3df5280d17c0a9ea5d541597fc
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-17 15:58:11 +0530

    final commit

--- HugeInteger.java
+++ HugeInteger.java
@@ -1,104 +1,346 @@
+import java.util.Arrays;
+
 public class HugeInteger {
-	int[] a = new int[40];
-	String s;
+	char[] a = new char[40];
+	int[] b = new int[40];
 
 	public HugeInteger() {
-		String s = "";
-		for (int i = 0; i < s.length(); i++)
+		for (int i = 0; i < 40; i++)
 			a[i] = 0;
-
 	}
 
 	public HugeInteger(String s) {
 		if (s.length() > 40)
 			throw new ArrayIndexOutOfBoundsException();
-		this.s = s;
+		parse(s);
 	}
 
-	public int[] parse(String s) {
-		for (int i = 0; i < s.length(); i++)
-			a[i] = s.charAt(i);
+	public char[] parse(String s) {
+		int i;
+		if (s.length() == 40) {
+			for (i = 0; i < s.length(); i++) {
+				a[i] = s.charAt(i);
+			}
+		}
+		// else {
+		// for (i = 0; i < 40 - s.length(); i++) {
+		// a[i] = 0;
+		// }
+		// for (i = 40 - s.length(), j = 0; i < 40 && j < s.length(); i++, j++) {
+		// a[i] = s.charAt(j);
+		// }
+		// }
+		charToInt();
 		return a;
 	}
 
-	public int[] add(HugeInteger h1, HugeInteger h2) {
-		int[] b = new int[40];
-		if (h1.parse(s).length > 40 || h2.parse(s).length > 40) {
-			System.out.println("Max. length after addition hould be 40-digits.");
-		}
-		for (int i = 0; i < h1.parse(s).length; i++) {
-			b[i] = h1.a[i] + h2.a[i];
-			if (b[i] > 9) {
-				b[i] -= 10;
-				b[i + 1] += 1;
-			}
-			if (b[parse(s).length - 1] > 9) {
-				System.out.println("Sum greater than 40-digits!!");
-			}
+	public int[] charToInt() {
+		for (int i = 0; i < a.length; i++) {
+			b[i] = (int) a[i] - 48;
 		}
 		return b;
 	}
 
-	// public int[] sub(HugeInteger h1, HugeInteger h2) {
-	// int[] b = new int[40];
-	// if (h1.parse(s).length > 40 || h2.parse(s).length > 40) {
-	// System.out.println("Max. length after addition hould be 40-digits.");
-	// }
-	//
-	// for (int i = 0; i < h1.parse(s).length; i++) {
-	// b[i] = h1.a[i] - h2.a[i];
-	// if (b[i] > 9) {
-	// b[i] -= 10;
-	// b[i + 1] += 1;
-	// }
-	// if (b[parse(s).length - 1] > 9) {
-	// System.out.println("Sum greater than 40-digits!!");
-	// }
-	// }
-	// return b;
-	// }
+	public char[] intToChar() {
+		for (int i = 0; i < b.length; i++) {
+			a[i] = (char) (b[i] + 48);
+		}
+		return a;
+	}
 
-	// public multiply() {
-	//
-	// }
-	//
-	// public divide() {
-	//
-	// }
-	//
-	// public remainder() {
-	//
-	// }
-	//
-	// public boolean isEqualTo() {
-	//
-	// }
-	//
-	// public boolean isNotEqualTo() {
-	//
-	// }
-	//
-	// public boolean isGreaterThan() {
-	//
-	// }
-	//
-	// public boolean isLessThan() {
-	//
-	// }
-	//
-	// public boolean isGreaterThanOrEqualTo() {
-	//
-	// }
-	//
-	// public boolean isLessThanOrEqualTo() {
-	//
-	// }
-	//
-	// public isZero() {
-	//
-	// }
+	public String ToString() {
+		String p = new String(a);
+		return p;
+	}
+
+	public int[] add(HugeInteger h1, HugeInteger h2) {
+		if (h1.b.length > 40 || h2.b.length > 40) {
+			System.out.println("Max. length after addition should be 40-digits.");
+		}
+		for (int i = h1.b.length - 1; i >= 0; i--) {
+			this.b[i] = h1.b[i] + h2.b[i];
+			if (i > 0) {
+				if (this.b[i] > 9) {
+					this.b[i] -= 10;
+					this.b[i - 1] += 1;
+				}
+			} else if (this.b[0] > 9) {
+				System.out.println("Sum greater than 40-digits!!");
+			}
+		}
+		// for (int i = 0; i < this.b.length; i++) {
+		// System.out.printf("%d", this.b[i]);
+		// }
+		// System.out.println();
+		intToChar();
+		return this.b;
+	}
+
+	public int[] sub(HugeInteger h1, HugeInteger h2) {
+		if (h1.b.length > 40 || h2.b.length > 40) {
+			System.out.println("Max. length after subtraction should be 40-digits.");
+		}
+		if (isGreaterThan(h1, h2)) {
+			for (int i = h1.b.length - 1; i >= 0; i--) {
+				if (h1.b[i] > h2.b[i]) {
+					this.b[i] = h1.b[i] - h2.b[i];
+				} else if (h1.b[i] < h2.b[i]) {
+					if (h1.b[i - 1] > 0) {
+						h1.b[i - 1] -= 1;
+						h1.b[i] += 10;
+					} else {
+						h1.b[i - 2] -= 1;
+						h1.b[i - 1] = 9;
+						h1.b[i] += 10;
+					}
+					this.b[i] = h1.b[i] - h2.b[i];
+				} else {
+					this.b[i] = 0;
+				}
+			}
+		} else {
+			for (int i = h1.b.length - 1; i >= 0; i--) {
+				if (h1.b[i] > h2.b[i]) {
+					this.b[i] = h1.b[i] - h2.b[i];
+				} else if (h1.b[i] < h2.b[i]) {
+					if (h1.b[i - 1] > 0) {
+						h1.b[i - 1] -= 1;
+						h1.b[i] += 10;
+					} else {
+						h1.b[i - 2] -= 1;
+						h1.b[i - 1] = 9;
+						h1.b[i] += 10;
+					}
+					this.b[i] = h1.b[i] - h2.b[i];
+				} else {
+					this.b[0] = 0;
+				}
+			}
+		}
+		intToChar();
+		return this.b;
+	}
+
+	public static boolean isEqualTo(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length == h2.b.length) {
+			while (i < h1.b.length) {
+				if (h1.b[i] == h2.b[i]) {
+					flag = 1;
+				} else {
+					flag = 0;
+					break;
+				}
+				i++;
+			}
+		}
+		if (flag == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isNotEqualTo(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length == h2.b.length) {
+			while (i < h1.b.length) {
+				if (h1.b[i] == h2.b[i]) {
+					flag = 1;
+				} else {
+					flag = 0;
+					break;
+				}
+				i++;
+			}
+		}
+		if (flag == 1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public static boolean isGreaterThan(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length > h2.b.length) {
+			return true;
+		} else if (h1.b.length < h2.b.length) {
+			return false;
+		} else {
+			while (i < h1.b.length) {
+				if (h1.b[i] > h2.b[i]) {
+					flag = 1;
+					break;
+				} else if (h1.b[i] < h2.b[i]) {
+					flag = 0;
+					break;
+				} else
+					i++;
+			}
+			if (flag == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static boolean isLessThan(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length > h2.b.length) {
+			return false;
+		} else if (h1.b.length < h2.b.length) {
+			return true;
+		} else {
+			while (i < h1.a.length) {
+				if (h1.b[i] > h2.b[i]) {
+					flag = 0;
+					break;
+				} else if (h1.b[i] < h2.b[i]) {
+					flag = 1;
+					break;
+				} else
+					i++;
+			}
+			if (flag == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static boolean isGreaterThanOrEqualTo(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length > h2.b.length) {
+			return true;
+		} else if (h1.b.length < h2.b.length) {
+			return false;
+		} else {
+			while (i < h1.b.length) {
+				if (h1.b[i] > h2.b[i]) {
+					flag = 1;
+					break;
+				} else if ((int) h1.b[i] < (int) h2.b[i]) {
+					flag = 0;
+					break;
+				} else if (h1.b[i] == h2.b[i]) {
+					flag = 2;
+				}
+				i++;
+			}
+			if (flag == 1 || flag == 2) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static boolean isLessThanOrEqualTo(HugeInteger h1, HugeInteger h2) {
+		int i = 0, flag = 0;
+		if (h1.b.length > h2.b.length) {
+			return false;
+		} else if (h1.b.length < h2.b.length) {
+			return true;
+		} else {
+			while (i < h1.b.length) {
+				if (h1.b[i] > h2.b[i]) {
+					flag = 0;
+					break;
+				} else if (h1.b[i] < h2.b[i]) {
+					flag = 1;
+					break;
+				} else if (h1.b[i] == h2.b[i]) {
+					flag = 2;
+				}
+				i++;
+			}
+			if (flag == 1 || flag == 2) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static boolean isZero(HugeInteger h) {
+		int i = 0, flag = 0;
+		while (i < h.b.length) {
+			if (h.b[i] == 0) {
+				flag = 0;
+			} else {
+				flag = 1;
+				break;
+			}
+			i++;
+		}
+		if (flag == 0)
+			return true;
+		else
+			return false;
+	}
 
 	public String toString() {
-
+		return String.format("%s", ToString());
 	}
 }
+
+// public String ToString() {
+// String s[] = new String[a.length];
+// for (int i = 0; i < a.length; i++) {
+// s[i] = String.valueOf(a[i]);
+// }
+// return (Arrays.toString(s));
+// }
+
+// public static boolean isEqualTo(HugeInteger h1, HugeInteger h2) {
+// int i = 0, flag = 0;
+// if (h1.a.length == h2.a.length) {
+// while (i < h1.a.length) {
+// if (h1.a[i] == h2.a[i]) {
+// flag = 1;
+// } else {
+// flag = 0;
+// break;
+// }
+// i++;
+// }
+// }
+// if (flag == 1) {
+// return true;
+// } else {
+// return false;
+// }
+// }
+
+// public multiply() {
+//
+//
+// }
+//
+// public int[] div(HugeInteger h1, HugeInteger h2) {
+// if(isGreaterThan(h1, h2)) {
+// while(isGreaterThan(sub(h1, h2),h2)) {
+// }
+// }
+// }
+//
+// public void rem(HugeInteger h1, HugeInteger h2) {
+// // remainder method
+// if (isGreaterThanOrEqualTo(h1, h2)) {
+// return sub(h1, h2);
+// } else {
+// // for (int i = 0; i < h1.b.length; i++) {
+// // System.out.printf("%d", h1.b[i]);
+// // }
+// // System.out.println();
+// return h1.b;
+// }
+// }
+
+// h.rem(h5, h4);
+// displayHugeInteger("Remainder of h5 & h4", h);
+// h.rem(h4, h5);
+// displayHugeInteger("Remainder of h4 & h5", h);
