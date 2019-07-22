5d7d3653bcaf4dcc45584dc0a993296356db7d4f
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-22 15:11:05 +0530

    commit

--- preIdentifiedCountries.java
+++ preIdentifiedCountries.java
@@ -1,3 +1,22 @@
 public enum preIdentifiedCountries {
+	Cuba("2005", "Heroine"), Mexico("2000", "Hash"), Canada("2010", "Pesticides"), Brazil("2006",
+			"Marijuana"), NewZealand("2014",
+					"Cocaine"), Pakistan("2002", "Weapons & Arms"), Singapore("2015", "Diamonds");
+
+	private final String yearOfEntry;
+	private final String cause;
+
+	private preIdentifiedCountries(String year, String cause) {
+		this.yearOfEntry = year;
+		this.cause = cause;
+	}
+
+	public String getYear() {
+		return yearOfEntry;
+	}
+
+	public String getCause() {
+		return cause;
+	}
 
 }
