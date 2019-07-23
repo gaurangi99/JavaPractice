eba11d3a14ef2dd8d12ce848eaec5a010b799b24
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-23 17:21:57 +0530

    commit

--- PassengerTest.java
+++ PassengerTest.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class PassengerTest {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Passenger p = new Passenger();
+		String name = new String();
+		String num = new String();
+		int issNum, cAP, cM, cW, aM, aW, aAP;
+		Date d = new Date();
+		System.out.println("\nEnter name of passenger:");
+		name = in.next();
+		p.setName(name);
+		System.out.println("\nEnter passport number:");
+		num = in.next();
+		System.out.println("\nEnter name of passenger:");
+		System.out.println("\nEnter name of passenger:");
+		System.out.println("\nEnter name of passenger:");
+
 		// TODO Auto-generated method stub
 
 	}
