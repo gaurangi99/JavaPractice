7b8ae8a4bab8f27e19b55ee0a9da9b37c940bed3
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-23 17:21:19 +0530

    commit

--- Date.java
+++ Date.java
@@ -4,6 +4,12 @@ public class Date {
 	private int year;
 	private static final int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	public Date() {
+		month = 0;
+		day = 0;
+		year = 0;
+	}
+
 	public Date(int month, int day, int year) {
 		if (month <= 0 || month > 12) {
 			throw new IllegalArgumentException("Month " + month + " is an invalid value!");
@@ -31,6 +37,27 @@ public class Date {
 
 	public int getYear() {
 		return year;
+	}
+
+	public void setDay(int day) {
+		if (day <= 0 || (day > daysPerMonth[month] && !(month == 2 && day == 29))) {
+			throw new IllegalArgumentException("Days (" + day + ") for month " + month + " are wrong!");
+		}
+		this.day = day;
+	}
+
+	public void setMonth(int month) {
+		if (month <= 0 || month > 12) {
+			throw new IllegalArgumentException("Month " + month + " is an invalid value!");
+		}
+		this.month = month;
+	}
+
+	public void setYear(int year) {
+		if (month == 2 && day == 29 && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+			throw new IllegalArgumentException("Year " + year + " is not a leap year!");
+		}
+		this.year = year;
 	}
 
 	// Q8.8
