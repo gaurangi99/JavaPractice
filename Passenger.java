61357ed09292ca59762c3a51a62ee976da714883
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-23 14:27:07 +0530

    commit

--- Passenger.java
+++ Passenger.java
@@ -1,23 +1,69 @@
 public class Passenger {
 	String name, action;
 	String[] prevDestn = new String[3];
-	int passNum, actualAgriPdt, claimAgriPdt, actualWpn, claimWpn, actualMoney;
+	int passNum, actualAgriPdt, claimAgriPdt, actualWpn, claimWpn, actualMoney, claimMoney;
 	Date passIss;
 	char extraMoneyCheck;
 
-	public Passenger(String name, int passNum, Date passIss, int claimAgriPdt, int claimWpn, String[] prevDestn) {
-		if (claimAgriPdt > 5) {
-
-		}
+	public Passenger(String name, int passNum, Date passIss, int claimAgriPdt, int claimWpn, int claimMoney,
+			String[] prevDestn) {
+		// if (claimAgriPdt > 5) {
+		//
+		// }
 		this.name = name;
 		this.passNum = passNum;
 		this.passIss = passIss;
 		this.claimAgriPdt = claimAgriPdt;
 		this.claimWpn = claimWpn;
+		this.claimMoney = claimMoney;
 		this.prevDestn = prevDestn;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getpassNum() {
+		return passNum;
+	}
+
+	public void setpassNum(int num) {
+		this.passNum = num;
+	}
+
+	public Date getpassIss() {
+		return passIss;
+	}
+
+	public void setpassIss(Date date) {
+		this.passIss = date;
+	}
+
+	public int getclaimAgriPdt() {
+		return claimAgriPdt;
+	}
+
+	public void setclaimAgriPdt(int pdt) {
+		this.claimAgriPdt = pdt;
+	}
+
+	public int getclaimWpn() {
+		return claimWpn;
+	}
+
+	public void setclaimWpn(int wpn) {
+		this.claimWpn = wpn;
+	}
+
+	public int getclaimMoney() {
+		return claimMoney;
+	}
+
+	public void setclaimMoney(int money) {
+		this.claimMoney = money;
 	}
 }
