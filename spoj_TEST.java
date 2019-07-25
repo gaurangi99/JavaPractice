593293da045c8f47c496dd78a86983e51becd73b
gaurangi99 <imgaurangi.99@gmail.com>
2019-07-25 23:36:11 +0530

    commit

---
+++ spoj_TEST.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class spoj_TEST {
+
+	public static void main(String[] args) {
+		int i = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println();
+		while (i != 42) {
+			i = in.nextInt();
+			if (i != 42)
+				System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+	}
+}
